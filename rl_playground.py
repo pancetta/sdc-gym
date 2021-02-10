c764283dbe34a75a7880dcb40b41f896ba03534c
janEbert <janpublicebert@posteo.net>
2021-02-10 09:34:11 +0100

    Add argument parsing
    Again, the code should work exactly the same as before.

--- rl_playground.py
+++ rl_playground.py
@@ -1,10 +1,123 @@
+import argparse
+
 import gym
-from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize
-from stable_baselines import PPO2, ACKTR
+import stable_baselines
 import matplotlib.pyplot as plt
 
 import sdc_gym
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--steps',
+        type=float,
+        default=10000,
+        help='Number of action steps to take in the environment.',
+    )
+    parser.add_argument(
+        '--M',
+        type=int,
+        default=3,
+        help=(
+            '"Difficulty" of the problem '
+            '(proportionally relates to nth-order differential equation)'
+            '(choose M = 3 or M = 5 for comparing MIN-preconditioner)'
+        ),
+    )
+    parser.add_argument(
+        '--restol',
+        type=float,
+        default=1E-10,
+        help=(
+            'Residual tolerance; '
+            'when residual is below this, stop iterating.'
+            'Lower means better solution but more iterations.'
+        ),
+    )
+    parser.add_argument(
+        '--envname',
+        type=str,
+        default='sdc-v0',
+        choices=['sdc-v0', 'sdc-v1'],
+        help=(
+            'Gym environment to use;\n    sdc-v0 – SDC with a full iteration '
+            'per step (no intermediate observations),\n    sdc-v1 – SDC with '
+            'a single iteration per step (with intermediate observations).'
+        ),
+    )
+    parser.add_argument(
+        '--learning_rate',
+        type=float,
+        # default for PPO2 in stable-baselines
+        default=25E-05,
+        help='Learning rate/step size of the model.',
+    )
+    parser.add_argument(
+        '--model_class',
+        type=str,
+        default='PPO2',
+        help='Class of model to instantiate.',
+    )
+    parser.add_argument(
+        '--policy_class',
+        type=str,
+        default='MlpPolicy',
+        help='Class of model policy.',
+    )
+    parser.add_argument(
+        '--tests',
+        type=float,
+        default=5000,
+        help='Number of test runs for each preconditioning method.',
+    )
+    return parser.parse_args()
+
+
+def _get_model_class(model_class_str):
+    """Return a model class according to `model_class_str`."""
+    try:
+        model_class = getattr(stable_baselines, model_class_str)
+    except AttributeError:
+        raise AttributeError(
+            f"could not find model class '{model_class_str}' "
+            f'in module `stable_baselines`'
+        )
+    assert issubclass(model_class, stable_baselines.common.BaseRLModel), \
+        ('model class must be a subclass of '
+         '`stable_baselines.common.BaseRLModel`')
+    return model_class
+
+
+def _get_policy_class(policy_class_str, model_class_str):
+    if model_class_str.upper() == 'DDPG':
+        policy_class_module = stable_baselines.ddpg.policies
+    elif model_class_str.upper() == 'DQN':
+        policy_class_module = stable_baselines.deepq.policies
+    else:
+        policy_class_module = stable_baselines.common.policies
+
+    try:
+        policy_class = getattr(
+            policy_class_module,
+            policy_class_str,
+        )
+    except AttributeError:
+        try:
+            policy_class = globals()[policy_class_str]
+        except KeyError:
+            raise AttributeError(
+                f"could not find policy class '{policy_class_str}' "
+                f'in module `stable_baselines.common.policies` '
+                f'or in this module'
+            )
+    assert issubclass(
+        policy_class,
+        stable_baselines.common.policies.BasePolicy,
+    ), ('policy class must be a subclass of '
+        '`stable_baselines.common.policies.BasePolicy`')
+    return policy_class
 
 
 def test_model(model, env, ntests, name):
@@ -54,10 +167,9 @@ def plot_results(results, color, label):
 
 
 def main():
+    args = parse_args()
+
     # Set parameters for SDC
-    # In order to compare to the MIN-preconditioner,
-    # choose M = 3 or M = 5.
-    M = 3
     # When to stop iterating (lower means better solution, but
     # more iterations)
     restol = args.restol
@@ -78,21 +190,27 @@ def main():
     env = VecNormalize(env, norm_obs=False, norm_reward=True)
 
     # Set up model
+    model_class = _get_model_class(args.model_class)
+    policy_class = _get_policy_class(args.policy_class, args.model_class)
+
     policy_kwargs = {}
-    model = PPO2(
-        MlpPolicy,
+
+    # Learning rate to try for PPO2: 1E-05
+    # Learning rate to try for ACKTR: 1E-03
+    learning_rate = args.learning_rate
+
+    model = model_class(
+        policy_class,
         env,
         verbose=1,
         policy_kwargs=policy_kwargs,
         tensorboard_log='./sdc_tensorboard/',
-        # Learning rate for PPO2: 1E-05
-        # Learning rate for ACKTR: 1E-03
-        # learning_rate=1E-5,
+        learning_rate=learning_rate,
     )
 
     # Train the model (need to put at least 100k steps to
     # see something)
-    model.learn(total_timesteps=int(10000))
+    model.learn(total_timesteps=int(args.steps))
 
     fname = 'sdc_model_acktr'
     model.save(fname)
@@ -104,8 +222,7 @@ def main():
     ntests = int(args.tests)
 
     # Load the trained agent for testing
-    # model = PPO2.load(fname)
-
+    # model = model_class.load(fname)
 
     # Test the trained model.
     env = gym.make(envname, M=M, dt=1.0, restol=restol)
