6d23ca68f50bb08f5265fb7060608634af72c12a
janEbert <janpublicebert@posteo.net>
2021-03-11 16:29:49 +0100

    Allow storing test statistics

--- rl_playground.py
+++ rl_playground.py
@@ -55,13 +55,31 @@ def setup_model(args, env):
     return model
 
 
-def test_model(model, env, ntests, name):
+def _store_test_stats(path, stats):
+    np.savez_compressed(path, **stats)
+
+
+def test_model(model, env, ntests, name, stats_path=None):
     """Test the `model` in the Gym `env` `ntests` times.
     `name` is the name for the test run for logging purposes.
+    `stats_path` is an optional path where to save statistics about
+    the test.
     """
     mean_niter = 0
     nsucc = 0
     results = []
+    stats = {
+        key: []
+        for key in [
+                'obs',
+                'action',
+                'reward',
+                'niter',
+                'lam',
+                'residual',
+                'terminal_observation',
+        ]
+    }
 
     num_envs = env.num_envs
     # Amount of test that will be ran in total
@@ -76,6 +94,9 @@ def test_model(model, env, ntests, name):
                               dtype=env.action_space.dtype)
 
         while not all(done):
+            if stats_path is not None:
+                stats['obs'].append(obs)
+
             # Do not predict an action when we would discard it anyway
             if env.envs[0].prec is None:
                 action, state = model.predict(
@@ -84,7 +105,15 @@ def test_model(model, env, ntests, name):
                     mask=done,
                     deterministic=True,
                 )
+
             obs, rewards, done, info = env.step(action)
+
+            if stats_path is not None:
+                stats['action'].append(action)
+                stats['reward'].append(rewards)
+                for info_ in info:
+                    for key in info_:
+                        stats[key].append(info_[key])
 
         for (env_, info_) in zip(env.envs, info):
             # We work on the info here because its information is
@@ -105,6 +134,10 @@ def test_model(model, env, ntests, name):
         mean_niter = 666
     print(f'{name:<3} -- Mean number of iterations and success rate: '
           f'{mean_niter:4.2f}, {nsucc / ntests_total * 100} %')
+
+    if stats_path is not None:
+        _store_test_stats(stats_path, stats)
+
     return results
 
 
@@ -118,11 +151,13 @@ def plot_results(results, color, label):
     )
 
 
-def run_tests(model, args, seed=None, fig_path=None):
+def run_tests(model, args, seed=None, fig_path=None, stats_path=None):
     """Run tests for the given `model` and `args`, using `seed` as the
     random seed.
 
     `fig_path` is an optional path to store result plots at.
+    `stats_path` is an optional path where to save statistics about the
+    reinforcement learning test.
     """
     policy_class = utils.get_policy_class(args.policy_class, args.model_class)
 
@@ -141,7 +176,8 @@ def run_tests(model, args, seed=None, fig_path=None):
     start_time = time.perf_counter()
     # Test the trained model.
     env = utils.make_env(args, num_envs=num_test_envs, seed=seed)
-    results_RL = test_model(model, env, ntests, 'RL')
+    results_RL = test_model(
+        model, env, ntests, 'RL', stats_path=stats_path)
 
     # Restart the whole thing, but now using the LU preconditioner (no RL here)
     # LU is serial and the de-facto standard. Beat this (or at least be on par)
